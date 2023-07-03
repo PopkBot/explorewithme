76da3f8ff762e5f80b450a63b62c1af96ec3632c
PopkBot <themrgenich@mail.ru>
2023-07-03 03:59:44 +0300

    fix: EventUpdateDto annotation

--- ewm-service/src/main/java/main/event/dto/EventUpdateDto.java
+++ ewm-service/src/main/java/main/event/dto/EventUpdateDto.java
@@ -1,12 +1,16 @@
 package main.event.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import main.access.Access;
 import main.event.State;
 
 @Data
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventUpdateDto extends EventInputDto {
     private State stateAction;
     private Access access;
